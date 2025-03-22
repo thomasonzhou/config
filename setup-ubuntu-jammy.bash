64a0a3e499e86125d7bd4c607f2f37ea7d6fd82d
Thomason Zhou <t95zhou@uwaterloo.ca>
2025-03-22 19:27:28 +0900

    Add git push

--- setup-ubuntu-jammy.bash
+++ setup-ubuntu-jammy.bash
@@ -40,5 +40,6 @@ git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
 git config --global alias.sw switch
+git config --global alias.p push
 git config --global user.name "Thomason Zhou"
 git config --global user.email "t95zhou@uwaterloo.ca"
