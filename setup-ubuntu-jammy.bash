823b491a2f1d08454abc1ac5056e017e24c7023a
Thomason Zhou <t95zhou@uwaterloo.ca>
2025-03-22 19:09:55 +0900

    Add more config steps (vscode)

--- setup-ubuntu-jammy.bash
+++ setup-ubuntu-jammy.bash
@@ -1,23 +1,23 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y git curl wget vim tmux terminator gparted \
 build-essential cmake \
-barrier firefox\
+barrier firefox \
 python3-pip python3-venv python3-setuptools python3-dev python3-rosdep
-pip3 install thefuck --user
 mkdir -p ~/src && cd ~/src
 git clone https://github.com/wting/autojump.git
 cd autojump && ./install.py
+pip3 install thefuck --user
 
-echo "[[ -s /Users/thomasonzhou/.autojump/etc/profile.d/autojump.sh ]] && source /Users/thomasonzhou/.autojump/etc/profile.d/autojump.sh" >> ~/.bashrc
+echo "[[ -s $HOME/.autojump/etc/profile.d/autojump.sh ]] && source $HOME/.autojump/etc/profile.d/autojump.sh" >> ~/.bashrc
 echo "export PATH=$PATH:$HOME/.local/bin" >> ~/.bashrc
 
+# ros2 humble
 locale  # check for UTF-8
-
-sudo apt update && sudo apt install locales
+sudo apt update && sudo apt install -y locales
 sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
-sudo apt install software-properties-common
+sudo apt install -y software-properties-common
 sudo add-apt-repository universe
 sudo apt update && sudo apt install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
@@ -27,3 +27,10 @@ sudo apt upgrade -y
 sudo apt install -y ros-humble-desktop
 sudo apt install -y ros-dev-tools
 echo ". /opt/ros/humble/setup.bash" >> ~/.bashrc
+
+# vscode
+sudo apt install -y software-properties-common apt-transport-https wget
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt update
+sudo apt install -y code
