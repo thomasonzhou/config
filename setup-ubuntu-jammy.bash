2ae507ce12c896819d719cd4ae8fe88a3634a74a
Thomason Zhou <t95zhou@uwaterloo.ca>
2025-03-23 15:14:59 +0900

    Add foxglove bridge

--- setup-ubuntu-jammy.bash
+++ setup-ubuntu-jammy.bash
@@ -12,6 +12,7 @@ echo '[[ -s $HOME/.autojump/etc/profile.d/autojump.sh ]] && source $HOME/.autoju
 echo 'export PATH=$PATH:$HOME/.local/bin' >> ~/.bashrc
 
 # ros2 humble
+export ROS_DISTRO=humble
 locale  # check for UTF-8
 sudo apt update && sudo apt install -y locales
 sudo locale-gen en_US en_US.UTF-8
@@ -19,14 +20,17 @@ sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 sudo apt install -y software-properties-common
 sudo add-apt-repository -y universe
-sudo apt update && sudo apt install curl -y
+sudo apt update && sudo apt install -y curl
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y ros-humble-desktop
+sudo apt install -y ros-$ROS_DISTRO-desktop
 sudo apt install -y ros-dev-tools
-echo '. /opt/ros/humble/setup.bash' >> ~/.bashrc
+echo '. /opt/ros/$ROS_DISTRO/setup.bash' >> ~/.bashrc
+
+# foxglove
+sudo apt install -y ros-$ROS_DISTRO-foxglove-bridge
 
 # vscode
 sudo apt-get install -y wget gpg
